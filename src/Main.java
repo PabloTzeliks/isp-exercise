7b1b2e442182a6567c31a7f38b4e338104dda1ec
PabloTzeliks <arq.pabloo@gmail.com>
2025-12-11 15:02:27 -0300

    feat: add the represantion of two 'IPedido' Objects for example of the Decorator Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-
+import model.IPedido;
+import model.PedidoBase;
+import service.concrete.FreteGratis;
+import service.concrete.FretePadrao;
 
 public class Main {
     public static void main(String[] args) {
+
+        IPedido p1 = new PedidoBase(1000);
+        p1 = new FretePadrao(p1, 34.25);
+
+        IPedido p2 = new PedidoBase(150);
+        p2 = new FreteGratis(p2);
+
+        System.out.println(p1.getNome());
+        System.out.println(p1.getValorFinal());
+
+        System.out.println();
+
+        System.out.println(p2.getNome());
+        System.out.println(p2.getValorFinal());
 
     }
 }
